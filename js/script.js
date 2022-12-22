bd084f2f3910c1d3e76a8dbdd2d0d6ce90487927
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-12-22 10:09:42 +0100

    Snack 1

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const { createApp } = Vue;
+
+createApp ( {
+    data() {
+        
+        return {
+            randomNumber : null,
+            oddNumberList: [],
+            evenNumberList: [],
+        }
+
+        
+    },
+
+    methods: {
+        
+    },
+
+    mounted(){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/int')
+            .then((response) => {
+                console.log(response.data);
+                this.randomNumber = response.data.response;
+                if (this.randomNumber % 2) {
+                    this.evenNumberList.push(this.randomNumber);
+                } else {
+                    this.oddNumberList.push(this.randomNumber);
+                }
+                
+            })
+            
+    },
+
+}).mount('#app');
+
+
+/**
+Creare due div; uno conterrà numeri dispari di colore rosso e l'altro conterrà numeri pari in verde.
+ Ad ogni click di un bottone chiedere all'API un numero, se è dispari metterlo nel blocco dispari, e se è pari in quello pari.
+*/
